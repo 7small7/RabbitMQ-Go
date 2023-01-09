449737441914370e128e874b53fbc837e1e4dedc
7small7 <1005349393@qq.com>
2023-01-10 00:29:16 +0800

    update

--- hello/consumer.go
+++ hello/consumer.go
@@ -1,1 +1,41 @@
-package main
+package hello
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"rabbit_mq/logs"
+)
+
+func Consumer() {
+	channel := connection()
+
+	// 声明一个队列
+	declare, err := channel.QueueDeclare("logs", false, false, false, false, nil)
+	if err != nil {
+		logs.FailOnError(err, "队列连接失败")
+	}
+
+	// 从队列中消费消息
+	consume, err := channel.Consume(
+		declare.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		logs.FailOnError(err, "消息获取失败")
+	}
+
+	// 从channel中获取数据(Consume方法返回的是一个channel)
+	val := <-consume
+	msg := make(map[string]interface{})
+	err = json.Unmarshal(val.Body, &msg)
+	fmt.Println(msg)
+	if err != nil {
+		logs.FailOnError(err, "消息反序列化失败")
+	}
+}
--- hello/producter.go
+++ hello/producter.go
@@ -1,1 +1,44 @@
-package main
+package hello
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/streadway/amqp"
+
+	"rabbit_mq/logs"
+)
+
+func Send() {
+	channel := connection()
+
+	// 1. 声明队列
+	declare, err := channel.QueueDeclare("logs", false, false, false, false, nil)
+	if err != nil {
+		logs.FailOnError(err, "Hello队列声明失败")
+	}
+
+	// 2. 组装和发送数据
+	marshal, err := json.Marshal(map[string]interface{}{
+		"name":   "公众号：菜鸟成长学习笔记",
+		"author": "kert",
+		"id":     1,
+	})
+	if err != nil {
+		logs.FailOnError(err, "消息JSON序列化失败")
+	}
+	err = channel.Publish(
+		"",
+		declare.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "text/plain", // 消息类型类型
+			Body:         marshal,      // 字节数组消息体
+			DeliveryMode: amqp.Persistent,
+		})
+	if err != nil {
+		logs.FailOnError(err, "消息发送失败")
+	}
+	fmt.Println("消息发送成功")
+}
